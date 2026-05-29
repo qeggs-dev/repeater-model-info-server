cc5df4ce31a5ce833f82f1b4fce65ea6abe051f2
Qeggs <get_boot@outlook.com>
2026-05-30 01:07:56 +0800

    feat(api): 添加模型查询异常处理
    当模型查询过程中发生SchemaError异常时，
    返回400状态码和错误信息，而不是让服务崩溃

--- info_server/api/apis/get_model/_get_model.py
+++ info_server/api/apis/get_model/_get_model.py
@@ -1,5 +1,6 @@
 from ..._server import Server
 from .._route import router
+from jsonschema import SchemaError
 from pydantic import BaseModel, Field
 from fastapi.responses import JSONResponse
 from ....model_api import Model
@@ -13,7 +14,15 @@ async def get_model_info(model_uid: str):
     """
     Get model info
     """
-    models = Server.core.find_models(model_uid)
+    try:
+        models = Server.core.find_models(model_uid)
+    except SchemaError as e:
+        return JSONResponse(
+            content = ModelInfoResponse(
+                message = str(e),
+            ).model_dump(exclude_none=True),
+            status_code=400,
+        )
     return JSONResponse(
         content = ModelInfoResponse(
             message = f"Get Model {model_uid} successfully",
